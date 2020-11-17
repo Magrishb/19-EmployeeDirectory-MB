54a1ccee77cc001ed8ebea48d5e3048afe4a5e97
Magrishb <bmagrish@gmail.com>
2020-11-16 23:22:50 -0600

    Added Jumbotron.js

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div id="page-header">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Our Cohort</h1>
+          <p class="lead">GT-ATL-FSF-FT-08-2020-U-C-MF</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
